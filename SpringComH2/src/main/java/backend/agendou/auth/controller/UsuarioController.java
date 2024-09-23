a350d3ef28a67eb1ad055d00c9b9f1370c45e1ba
Henrique-Crispino <henrique.crispino@sptech.school>
2024-09-23 19:20:29 -0300

    Refact: Correção da duplicidade dos endpoints login

--- SpringComH2/src/main/java/backend/agendou/auth/controller/UsuarioController.java
+++ SpringComH2/src/main/java/backend/agendou/auth/controller/UsuarioController.java
@@ -21,16 +21,6 @@ public class UsuarioController {
     @Autowired
     private UsuarioService service;
 
-    @GetMapping("/login")
-    public ResponseEntity<String> login(@RequestParam String email, @RequestParam String senha) {
-        System.out.println("Recebida requisição de login com email: " + email);
-        try {
-            return service.login(email, senha);
-        } catch (Exception e) {
-            return ResponseEntity.status(401).body("E-mail ou senha inválido.");
-        }
-    }
-
     @GetMapping("/listar")
     public ResponseEntity<List<UsuarioResponseDTO>> listarUsuarios() {
         try {
