089f409671e13e25b5cb6fac31a1b0d45e685ce2
MariaGirote <maria.nascimento@sptech.school>
2024-11-26 18:32:38 -0300

    atualizei tipo na classe service

--- src/main/java/back/domain/dto/request/ServicosVinculadosRequestDTO.java
+++ src/main/java/back/domain/dto/request/ServicosVinculadosRequestDTO.java
@@ -1,5 +1,6 @@
 package back.domain.dto.request;
 
+import back.domain.model.Funcionario;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,6 @@ import lombok.NoArgsConstructor;
 @Data
 public class ServicosVinculadosRequestDTO {
     private Integer id;
-    private Integer fk_funcionario;
+    private Funcionario fk_funcionario;
     private Integer fk_servico;
 }
